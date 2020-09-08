f3c4ca2825002d285c37ae7085ab9a94869a8190
Lars Behrenberg <l.behrenberg@gmail.com>
2020-09-08 20:08:41 +0900

    Implement button hover effect

--- src/components/Index/testimonials.js
+++ src/components/Index/testimonials.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import Img from 'gatsby-image';
+import React from 'react'
+import styled from '@emotion/styled'
+import Img from 'gatsby-image'
+import { Link } from 'gatsby'
 
 const Container = styled.div`
   max-width: 800px;
-`;
+`
 
 const NameHeader = styled.h1`
   font-size: 3rem;
   font-weight: 400;
-`;
+`
 
 const Subtitle = styled.p`
   margin-bottom: 0.3rem;
@@ -17,7 +18,7 @@ const Subtitle = styled.p`
   font-size: 1.4rem;
   border-left: 2px solid white;
   padding-left: 0.8rem;
-`;
+`
 
 const TestimonialContainer = styled.section`
   flex-direction: column;
@@ -35,7 +36,7 @@ const TestimonialContainer = styled.section`
       border-radius: 0;
     }
   }
-`;
+`
 
 const Image = styled.div`
   grid-row: 1 / 4;
@@ -47,7 +48,7 @@ const Image = styled.div`
     border-radius: 0.4rem 0 0 0.4rem;
     max-height: 330px;
   }
-`;
+`
 
 const Text = styled.div`
   line-height: 1.5;
@@ -61,9 +62,9 @@ const Text = styled.div`
   h2 {
     margin-bottom: 0.4rem;
   }
-`;
+`
 
-const Button = styled.button`
+const Button = styled(Link)`
   -webkit-font-smoothing: antialiased;
   box-sizing: inherit;
   font-family: inherit;
@@ -96,7 +97,35 @@ const Button = styled.button`
   grid-row: 3 / 3;
   grid-column: 5 / 7;
   padding: 32px;
-`;
+
+  span {
+    display: inline-block;
+    position: relative;
+    transition: all 300ms ease-out;
+    will-change: transform;
+  }
+  &:hover span {
+    transform: translate3d(-0.5rem, 0, 0);
+  }
+  svg {
+    position: absolute;
+    width: 1.1em;
+    right: 0px;
+    right: 0rem;
+    opacity: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    transition: all 300ms ease-out;
+    will-change: right, opacity;
+    * {
+      stroke-width: 5;
+    }
+  }
+  &:hover svg {
+    opacity: 1;
+    right: -1.5rem;
+  }
+`
 const Testimonials = ({ testimonial }) => (
   <>
     <Container>
@@ -121,7 +150,31 @@ const Testimonials = ({ testimonial }) => (
           style={{ objectFit: 'fit' }}
         />
       </Image>
-      <Button>Next</Button>
+      <Button to="/latest-work">
+        <span>
+          Next
+          <svg
+            width="10"
+            height="10"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ marginRight: '5px', display: 'inline-block' }}
+          >
+            <g clipPath="url(#clip0)">
+              <path
+                d="M15.2894 10.9901L6.6897 19.5895C6.14266 20.1368 5.25572 20.1368 4.70894 19.5895C4.16212 19.0427 4.16212 18.1558 4.70894 17.609L12.3183 9.99986L4.70917 2.39095C4.16234 1.84391 4.16234 0.957105 4.70917 0.410283C5.25599 -0.136761 6.14288 -0.136761 6.68992 0.410283L15.2896 9.00983C15.563 9.28337 15.6996 9.64151 15.6996 9.99981C15.6996 10.3583 15.5628 10.7167 15.2894 10.9901Z"
+                fill="white"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0">
+                <rect width="20" height="20" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+        </span>
+      </Button>
       <Text>
         <h2>{testimonial.frontmatter.title}</h2>
         <h4>{testimonial.frontmatter.subtitle}</h4>
@@ -129,6 +182,6 @@ const Testimonials = ({ testimonial }) => (
       </Text>
     </TestimonialContainer>
   </>
-);
+)
 
-export default Testimonials;
+export default Testimonials
